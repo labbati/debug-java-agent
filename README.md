279b4b04c59ae5f1e3311dce20190c2ea4938853
Luca Abbati <luca.abbati@gmail.com>
2019-04-08 19:11:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+    java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=6789 build/libs/debugging-java-manually-1.0-SNAPSHOT.jar
---
+++ src/main/java/com/labbati/blog/debug/ManualDebugApp.java
@@ -0,0 +1,8 @@
+package com.labbati.blog.debug;
+
+public class ManualDebugApp {
+
+    public static void main(String[] args) {
+        System.out.println("This is mainn");
+    }
+}
